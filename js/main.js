38149d661c2295f604bbefa165c4582285925dd9
EugeBr <eugebordenave@gmail.com>
2023-10-10 13:13:06 +0200

    Methods and Events

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 const API = "https://api.github.com/users/";
 
-async function doSearch() {
-    const response = await fetch(API + 'EugeBr');
-    const data = await response.json();
-    console.log(data);
-}
 
 const app = Vue.createApp({
     data() {
         return {
             message: 'Hello Vue!'
         };
+    },
+    methods: {
+        async doSearch() {
+            const response = await fetch(API + 'EugeBr');
+            const data = await response.json();
+            console.log(data);
+        }
     }
 });
 
