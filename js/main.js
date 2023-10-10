93122263345484dd78776f2a00b26ff81f1b052e
EugeBr <eugebordenave@gmail.com>
2023-10-10 18:09:28 +0200

    Computed properties

--- js/main.js
+++ js/main.js
@@ -10,16 +10,21 @@ const app = Vue.createApp({
             favorites: new Map()
         };
     },
+    computed: {
+        isFavorite() {
+            return this.favorites.has(this.result.id);
+        }
+    },
     methods: {
         async doSearch() {
             this.result = this.error = null
             try {
                 const response = await fetch(API + this.search);
-                if(!response.ok) throw new Error('User not found');
+                if (!response.ok) throw new Error('User not found');
                 const data = await response.json();
                 //console.log(data);
                 this.result = data;
-            } catch (error){
+            } catch (error) {
                 this.error = error;
             } finally {
                 this.search = null;
@@ -28,6 +33,10 @@ const app = Vue.createApp({
 
         addFavorite() {
             this.favorites.set(this.result.id, this.result);
+        },
+
+        removeFavorite() {
+            this.favorites.delete(this.result.id);
         }
     }
 });
